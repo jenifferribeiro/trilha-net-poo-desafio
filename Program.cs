0c358258f62639ef1ef6cdf7cf0ec53a7e6dc415
Jeniffer Ribeiro <js.ribei@hotmail.com>
2024-01-29 19:22:47 -0300

    Implementando Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("11-9999-9999", "g11", "12345678", 128);
+
+        Console.WriteLine(nokia);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Linkedìn");
+
+
+        Iphone iphone = new Iphone ("11-8888-8888", "15", "12345698", 256);
+
+        Console.WriteLine(iphone);
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+        
+    }
+}
